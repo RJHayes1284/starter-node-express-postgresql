f6c127b08ee27204ed9ca71f3a40bc6870b40ff7
Rebekah Jeane Hayes <rebekahjhayes@gmail.com>
2023-02-06 22:19:16 -0600

    lesson

--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,17 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+const path = require("path");
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
 module.exports = {
-    development: {
-      client: "postgresql",
-      connection: DATABASE_URL,
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
     },
-  }
+  },
+};
